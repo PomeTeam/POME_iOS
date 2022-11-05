9eacdbd717a983b6f64a23c922243ff19cce1fce
Ssooya <bsy24660@naver.com>
2022-11-05 15:31:05 +0900

    #3: UIColor extension 파일 추가 및 프로젝트 Color 세팅

---
+++ POME/global/Resource/Extension/UIColor.swift
@@ -0,0 +1,61 @@
+//
+//  UIColor.swift
+//  POME
+//
+//  Created by 박지윤 on 2022/11/05.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    
+    //MARK: - Mint
+    static let main = UIColor(red: 0.482, green: 0.894, blue: 0.561, alpha: 1)
+    
+    static let middle_mint = UIColor(red: 0.528, green: 0.913, blue: 0.602, alpha: 1)
+    
+    static let disabled_mint = UIColor(red: 0.69, green: 0.937, blue: 0.737, alpha: 1)
+    
+    static let light_mint = UIColor(red: 0.885, green: 0.988, blue: 0.904, alpha: 1)
+    
+    static let bright_mint = UIColor(red: 0.95, green: 1, blue: 0.96, alpha: 1)
+    
+    //MARK: - Pink
+    static let sub = UIColor(red: 1, green: 0.565, blue: 0.541, alpha: 1)
+    
+    static let middle_pink = UIColor(red: 1, green: 0.866, blue: 0.854, alpha: 1)
+    
+    static let light_pink = UIColor(red: 1, green: 0.957, blue: 0.953, alpha: 1)
+    
+    //MARK: - Grey
+    static let grey_0 = UIColor(red: 1, green: 0.565, blue: 0.541, alpha: 1)
+    
+    static let grey_1 = UIColor(red: 0.973, green: 0.98, blue: 0.984, alpha: 1)
+    
+    static let grey_2 = UIColor(red: 0.949, green: 0.961, blue: 0.961, alpha: 1)
+    
+    static let grey_3 = UIColor(red: 0.886, green: 0.894, blue: 0.902, alpha: 1)
+    
+    static let grey_4 = UIColor(red: 0.812, green: 0.827, blue: 0.839, alpha: 1)
+    
+    static let grey_5 = UIColor(red: 0.667, green: 0.686, blue: 0.702, alpha: 1)
+    
+    static let grey_6 = UIColor(red: 0.533, green: 0.553, blue: 0.569, alpha: 1)
+    
+    static let grey_7 = UIColor(red: 0.306, green: 0.322, blue: 0.337, alpha: 1)
+    
+    static let grey_8 = UIColor(red: 0.216, green: 0.227, blue: 0.235, alpha: 1)
+    
+    static let grey_9 = UIColor(red: 0.161, green: 0.165, blue: 0.169, alpha: 1)
+    
+    //MARK: - Etc
+    static let red = UIColor(red: 0.983, green: 0.369, blue: 0.369, alpha: 1)
+    
+    static let shadow = UIColor(red: 0.427, green: 0.553, blue: 0.678, alpha: 1)
+    
+    static let blue_action_sheet = UIColor(red: 0.063, green: 0.51, blue: 1, alpha: 1)
+    
+    static let transparent = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0)
+    
+}
