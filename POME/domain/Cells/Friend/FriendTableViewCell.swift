5cac1ca2284ed87508c60b54036411a96abfbef2
Ssooya <bsy24660@naver.com>
2022-11-07 16:06:15 +0900

    #11: 친구 목록 타이틀 및 CollectionView 기본 UI 구현

--- POME/domain/Cells/Friend/FriendTableViewCell.swift
+++ POME/domain/Cells/Friend/FriendTableViewCell.swift
@@ -7,7 +7,24 @@
 
 import UIKit
 
-class FriendTableViewCell: UITableViewCell {
+class FriendTableViewCell: BaseTableViewCell {
 
+    static let cellIdentifier = "FriendTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func hierarchy() {
+        super.hierarchy()
+    }
+    
+    override func layout() {
+        super.layout()
+    }
 
 }
--- POME/domain/Views/Friend/FriendView.swift
+++ POME/domain/Views/Friend/FriendView.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import UIKit
+
+class FriendView: BaseView{
+    
+    //MARK: - UI
+    
+    let titleLabel = UILabel().then{
+        $0.text = "친구 응원하기"
+        $0.font = UIFont.autoPretendard(type: .b_18)
+        $0.textColor = Color.grey_9
+    }
+    
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then{
+        
+        let flowLayout = UICollectionViewFlowLayout().then{
+            $0.minimumLineSpacing = 18
+            $0.minimumInteritemSpacing = 18
+            $0.itemSize = CGSize(width: 52, height: 96)
+            $0.scrollDirection = .horizontal
+        }
+        
+        $0.collectionViewLayout = flowLayout
+        $0.showsHorizontalScrollIndicator = false
+        $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        
+        $0.register(FriendCollectionViewCell.self, forCellWithReuseIdentifier: FriendCollectionViewCell.cellIdentifier)
+        
+    }
+    
+    let tableView = UITableView().then{
+        $0.register(FriendTableViewCell.self, forCellReuseIdentifier: FriendTableViewCell.cellIdentifier)
+    }
+    
+    //MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Method
+    override func hierarchy() {
+        self.addSubview(titleLabel)
+        self.addSubview(collectionView)
+        self.addSubview(tableView)
+    }
+    
+    override func constraint() {
+        
+        titleLabel.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        collectionView.snp.makeConstraints{
+            $0.height.equalTo(96)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        tableView.snp.makeConstraints{
+            $0.top.equalTo(collectionView.snp.bottom).offset(7)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
