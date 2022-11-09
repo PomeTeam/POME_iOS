f7aaf0baefcfd91fd52607bed589d172fa107c14
gom1n <alice436777@gmail.com>
2022-11-09 12:32:17 +0900

    #23 : 닉네임 텍스트필드 로직 구현

--- POME/domain/ViewControllers/Register/RegisterViewController.swift
+++ POME/domain/ViewControllers/Register/RegisterViewController.swift
@@ -8,50 +8,15 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-    let registerTitleLabel = UILabel().then{
-        $0.text = "나만의 프로필을\n만들어볼까요?"
-        $0.textColor = Color.grey_9
-        $0.font = UIFont.autoPretendard(type: .b_24)
-        $0.numberOfLines = 0
-    }
-    let profileImage = UIImageView().then{
-        $0.image = Image.photoDefault
-        $0.isUserInteractionEnabled = true
-        $0.layer.masksToBounds = true
-        $0.contentMode = .scaleAspectFill
-    }
+    
+    // MARK: - Properties
+    var registerView: RegisterView!
     let maskView = UIImageView()
-    let profileButton = UIButton().then{
-        $0.setImage(Image.plus, for: .normal)
-    }
-    let nameTextField = UITextField().then{
-        $0.backgroundColor = Color.grey_0
-        $0.placeholder = "영어, 한국어 최대 10자 이내 입력"
-        $0.clearButtonMode = .always
-        $0.font = UIFont.autoPretendard(type: .m_16)
-        $0.textColor = Color.grey_9
-        $0.layer.cornerRadius = 6
-        $0.addLeftPadding(16)
-    }
-    let messageLabel = UILabel().then{
-        $0.text = "멋진 닉네임이네요!"
-        $0.font = UIFont.autoPretendard(type: .m_14)
-        $0.textColor = Color.main
-    }
-    let guideLabel = UILabel().then{
-        $0.text = "입력한 정보는 회원을 식별하고 친구간 커뮤니케이션을 위한\n동의 목적으로만 사용되며 포미 이용기간 동안 보관돼요"
-        $0.font = UIFont.autoPretendard(type: .m_12)
-        $0.textColor = Color.grey_5
-        $0.numberOfLines = 0
-        $0.textAlignment = .center
-    }
-    var completeButton = DefaultButton(titleStr: "입력했어요").then{
-        $0.isActivate(false)
-    }
-
     let imagePickerController = UIImagePickerController()
     var selectedPhoto: UIImage!
+    var name: String!
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -60,65 +25,31 @@ class RegisterViewController: UIViewController {
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        maskView.frame = profileImage.bounds
+        maskView.frame = registerView.profileImage.bounds
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
+    // MARK: - Methods
     func layout() {
-        self.view.addSubview(registerTitleLabel)
-        self.view.addSubview(profileImage)
-        self.view.addSubview(profileButton)
-        
-        self.view.addSubview(nameTextField)
-        self.view.addSubview(messageLabel)
-        
-        self.view.addSubview(completeButton)
-        self.view.addSubview(guideLabel)
-        
-        registerTitleLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(16)
-            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(56)
-        }
-        profileImage.snp.makeConstraints { make in
-            make.top.equalTo(registerTitleLabel.snp.bottom).offset(44)
-            make.width.height.equalTo(160)
-            make.centerX.equalToSuperview()
-        }
-        profileButton.snp.makeConstraints { make in
-            make.width.height.equalTo(24)
-            make.trailing.equalTo(profileImage)
-            make.bottom.equalTo(profileImage).offset(-7)
-        }
-        
-        nameTextField.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.leading.trailing.equalToSuperview().inset(16)
-            make.top.equalTo(profileImage.snp.bottom).offset(40)
-        }
-        messageLabel.snp.makeConstraints { make in
-            make.top.equalTo(nameTextField.snp.bottom).offset(10)
-            make.leading.equalTo(nameTextField)
-        }
-        
-        completeButton.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.leading.trailing.equalToSuperview().inset(16)
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-14)
-        }
-        guideLabel.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(42)
-            make.bottom.equalTo(completeButton.snp.top).offset(-20)
+        registerView = RegisterView()
+        self.view.addSubview(registerView)
+        registerView.snp.makeConstraints { make in
+            make.leading.trailing.top.bottom.equalToSuperview()
         }
     }
     func initialize() {
         maskView.image = Image.photoDefault
-        profileImage.mask = maskView
+        registerView.profileImage.mask = maskView
         // imagePicker delegate
         imagePickerController.delegate = self
         
-        completeButton.addTarget(self, action: #selector(completeButtonDidTap), for: .touchUpInside)
+        registerView.completeButton.addTarget(self, action: #selector(completeButtonDidTap), for: .touchUpInside)
+        registerView.nameTextField.addTarget(self, action: #selector(nameTextFieldEditingChanged), for: .editingChanged)
         
-        profileButton.addTarget(self, action: #selector(albumButtonnDidTap), for: .touchUpInside)
-        profileImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.albumButtonnDidTap)))
+        registerView.profileButton.addTarget(self, action: #selector(albumButtonnDidTap), for: .touchUpInside)
+        registerView.profileImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.albumButtonnDidTap)))
     }
 
     @objc func completeButtonDidTap() {
@@ -128,6 +59,27 @@ class RegisterViewController: UIViewController {
         self.imagePickerController.sourceType = .photoLibrary
         self.present(imagePickerController, animated: true, completion: nil)
     }
+    @objc func nameTextFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.name = text
+        checkValidName(self.name)
+    }
+    func checkValidName(_ name: String) {
+        if name.count > 0 {
+            registerView.messageLabel.then{
+                $0.text = "멋진 닉네임이네요!"
+                $0.textColor = Color.main
+            }
+            registerView.completeButton.isActivate(true)
+        } else {
+            registerView.messageLabel.then{
+                $0.text = "사용할 수 없는 닉네임이에요"
+                $0.textColor = Color.red
+            }
+            registerView.completeButton.isActivate(false)
+        }
+        registerView.messageLabel.isHidden = false
+    }
 }
 // MARK: - ImagePicker Delegate
 extension RegisterViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -135,7 +87,7 @@ extension RegisterViewController : UIImagePickerControllerDelegate, UINavigation
         self.selectedPhoto = UIImage()
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             self.selectedPhoto = image
-            self.profileImage.image = image
+            self.registerView.profileImage.image = image
         }
         self.dismiss(animated: true, completion: nil)
     }
---
+++ POME/global/Source/Base/BaseLabel.swift
@@ -0,0 +1,34 @@
+//
+//  BaseLabel.swift
+//  POME
+//
+//  Created by gomin on 2022/11/09.
+//
+
+import Foundation
+import UIKit
+
+class BaseLabel: UILabel {
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    override func drawText(in rect: CGRect) {
+        if let text = text {
+            let attrString = NSMutableAttributedString(string: text)
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 4
+            attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attrString.length))
+            self.attributedText = attrString
+        }
+        self.textAlignment = .center
+        
+        super.drawText(in: rect)
+    }
+}
