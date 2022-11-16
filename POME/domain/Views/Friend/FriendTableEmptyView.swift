c8d48baffbcf173408b8c0422310a09b18ab5154
Ssooya <bsy24660@naver.com>
2022-11-17 08:54:36 +0900

    #11: 친구탭 파일 추가

---
+++ POME/domain/Views/Friend/FriendTableEmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  FriendTableEmptyView.swift
+//  POME
+//
+//  Created by 박지윤 on 2022/11/17.
+//
+
+import UIKit
+
+class FriendTableEmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
