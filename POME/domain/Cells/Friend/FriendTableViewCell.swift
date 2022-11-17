c7832afb88d58fcb5783c3b538cc1f73a9e4806e
Ssooya <bsy24660@naver.com>
2022-11-17 13:13:00 +0900

    #11: 친구TableViewCell 기본 UI 구현

--- POME/domain/Cells/Friend/FriendTableViewCell.swift
+++ POME/domain/Cells/Friend/FriendTableViewCell.swift
@@ -17,6 +17,8 @@ class FriendTableViewCell: BaseTableViewCell {
     
     //MARK: - UI
     
+    let marginView = UIView()
+    
     let profileImage = UIImageView().then{
         $0.backgroundColor = Color.grey4
     }
@@ -30,26 +32,24 @@ class FriendTableViewCell: BaseTableViewCell {
     
     let nameLabel = UILabel().then{
         $0.text = "eunjoeme_xx"
-        $0.font = UIFont.autoPretendard(type: .m_14)
-        $0.textColor = Color.grey6
+        $0.textColor = Color.body
+        $0.setTypoStyle(typoStyle: .subtitle3)
     }
+    
     let tagLabel = PaddingLabel().then{
-        $0.text = "커피 대신 물을 마시자"
-        $0.font = UIFont.autoPretendard(type: .m_12)
+        $0.text = "· 커피 대신 물을 마시자"
         $0.textColor = Color.grey5
-        $0.backgroundColor = Color.grey1
-        $0.layer.cornerRadius = 4
-        $0.clipsToBounds = true
+        $0.setTypoStyle(typoStyle: .body3)
     }
     
     let timeLabel = UILabel().then{
-        $0.text = " 1시간 전"
-        $0.font = UIFont.autoPretendard(type: .m_12)
+        $0.text = "· 44분 전"
         $0.textColor = Color.grey5
+        $0.setTypoStyle(typoStyle: .body3)
     }
 
     let emotionStackView = UIStackView().then{
-        $0.spacing = -6
+        $0.spacing = 2
         $0.axis = .horizontal
     }
     
@@ -61,21 +61,24 @@ class FriendTableViewCell: BaseTableViewCell {
         $0.backgroundColor = Color.pink100
     }
     
-    let arrowImage = UIImage().then{
-        $0.image = Image.arrow
+    let tagArrowBackgroundView = UIView()
+    
+    let tagArrowImage = UIImageView().then{
+        $0.image = Image.tagArrow
     }
     
     //
     let priceLabel = UILabel().then{
         $0.text = "320,800원"
-        $0.font = UIFont.autoPretendard(type: .sb_18)
         $0.textColor = Color.title
+        $0.setTypoStyle(typoStyle: .title3)
     }
     let memoLabel = UILabel().then{
-        $0.text = "여기에 멀 치면 어케 나오나요 대박킼"
-        $0.setTypoStyle(typoStyle: .body2)
+        $0.text = "아휴 힘빠져 이젠 진짜 포기다 포기 도대체 뭐가 문제일까 현실을 되돌아볼 필요를 느낀다ㅠ 이정도 노력했으···"
         $0.textColor = Color.title
+        $0.setTypoStyle(typoStyle: .body2)
         $0.numberOfLines = 2
+        $0.backgroundColor = .red
     }
     
     //
@@ -93,7 +96,9 @@ class FriendTableViewCell: BaseTableViewCell {
         $0.backgroundColor = Color.pink100
     }
     
-    lazy var etcButton = UIButton()
+    lazy var moreButton = UIButton().then{
+        $0.setImage(Image.more, for: .normal)
+    }
     
     //
     let separatorLine = UIView().then{
@@ -121,97 +126,103 @@ class FriendTableViewCell: BaseTableViewCell {
     }
     
     //MARK: - Override
-    override func setting() {
-        
-        super.setting()
-        
-        self.baseView.setShadowStyle(type: .friendCard)
-    }
     
     override func hierarchy() {
         
         super.hierarchy()
         
         self.baseView.addSubview(profileImage)
+        self.baseView.addSubview(marginView)
+        self.baseView.addSubview(separatorLine)
         
-        self.baseView.addSubview(nameLabel)
-        self.baseView.addSubview(priceLabel)
-        self.baseView.addSubview(memoLabel)
-        self.baseView.addSubview(tagLabel)
+        marginView.addSubview(topStackView)
+        marginView.addSubview(emotionStackView)
+        marginView.addSubview(priceLabel)
+        marginView.addSubview(memoLabel)
+        marginView.addSubview(reactionStackView)
+        marginView.addSubview(moreButton)
         
-        self.baseView.addSubview(timeLabel)
-        self.baseView.addSubview(emotionStackView)
-        self.baseView.addSubview(reactionStackView)
+        topStackView.addArrangedSubview(nameLabel)
+        topStackView.addArrangedSubview(tagLabel)
+        topStackView.addArrangedSubview(timeLabel)
         
-        emotionStackView.insertArrangedSubview(reviewEmotionTag, at: 0)
-        emotionStackView.insertArrangedSubview(firstEmotionTag, at: 0)
+        emotionStackView.addArrangedSubview(firstEmotionTag)
+        emotionStackView.addArrangedSubview(tagArrowBackgroundView)
+        tagArrowBackgroundView.addSubview(tagArrowImage)
+        emotionStackView.addArrangedSubview(reviewEmotionTag)
 
         reactionStackView.insertArrangedSubview(othersReactionButton, at: 0)
         reactionStackView.insertArrangedSubview(myReactionBtn, at: 0)
     }
     
     override func layout() {
+        
+        super.layout()
 
-        self.baseView.snp.remakeConstraints{
-            $0.height.equalTo(161 + 14)
-            $0.top.equalToSuperview().offset(7)
-            $0.leading.equalToSuperview().offset(16)
-            $0.trailing.equalToSuperview().offset(-16)
-            $0.centerY.equalToSuperview()
+        self.baseView.snp.makeConstraints{
+            $0.height.equalTo(214)
         }
         
         profileImage.snp.makeConstraints{
-            $0.top.leading.equalToSuperview().offset(16)
-            $0.width.height.equalTo(32)
+            $0.top.equalToSuperview().offset(20)
+            $0.leading.equalToSuperview().offset(24)
+            $0.width.height.equalTo(30)
         }
         
-        nameLabel.snp.makeConstraints{
+        marginView.snp.makeConstraints{
             $0.top.equalToSuperview().offset(20)
             $0.leading.equalTo(profileImage.snp.trailing).offset(8)
-        }
-        
-        priceLabel.snp.makeConstraints{
-            $0.top.equalTo(nameLabel.snp.bottom).offset(10)
-            $0.leading.equalTo(nameLabel)
-        }
-        
-        memoLabel.snp.makeConstraints{
-            $0.top.equalTo(priceLabel.snp.bottom).offset(6)
-            $0.leading.equalTo(nameLabel)
-            $0.trailing.equalToSuperview().offset(-126)
-            $0.height.equalTo(37)
-        }
-        
-        tagLabel.snp.makeConstraints{
-            $0.top.equalTo(memoLabel.snp.bottom).offset(8)
-            $0.leading.equalTo(nameLabel)
-            $0.height.equalTo(22)
+            $0.trailing.equalToSuperview().offset(-24)
             $0.bottom.equalToSuperview().offset(-20)
         }
         
-        timeLabel.snp.makeConstraints{
-            $0.top.equalTo(nameLabel)
-            $0.trailing.equalToSuperview().offset(-16)
+        topStackView.snp.makeConstraints{
+            $0.top.leading.equalToSuperview()
+            $0.trailing.lessThanOrEqualToSuperview()
         }
-        
+    
         emotionStackView.snp.makeConstraints{
-            $0.top.equalTo(timeLabel.snp.bottom).offset(14)
-            $0.trailing.equalTo(timeLabel)
+            $0.top.equalTo(topStackView.snp.bottom).offset(8)
+            $0.height.equalTo(26)
+            $0.leading.equalToSuperview()
+            $0.trailing.lessThanOrEqualToSuperview()
         }
         
         firstEmotionTag.snp.makeConstraints{
-            $0.width.height.equalTo(34)
+            $0.height.equalToSuperview()
+            $0.width.equalTo(70)
+        }
+        
+        tagArrowBackgroundView.snp.makeConstraints{
+            $0.width.equalTo(tagArrowImage)
+        }
+        
+        tagArrowImage.snp.makeConstraints{
+            $0.width.height.equalTo(12)
+            $0.centerY.equalToSuperview()
         }
         
         reviewEmotionTag.snp.makeConstraints{
-            $0.width.height.equalTo(34)
+            $0.height.equalToSuperview()
+            $0.width.equalTo(70)
+        }
+        
+        priceLabel.snp.makeConstraints{
+            $0.top.equalTo(emotionStackView.snp.bottom).offset(12)
+            $0.leading.equalToSuperview()
+            $0.trailing.lessThanOrEqualToSuperview()
+        }
+        
+        memoLabel.snp.makeConstraints{
+            $0.top.equalTo(priceLabel.snp.bottom).offset(4)
+            $0.leading.equalToSuperview()
+            $0.trailing.lessThanOrEqualToSuperview()
         }
         
         reactionStackView.snp.makeConstraints{
-            $0.top.equalTo(emotionStackView.snp.bottom).offset(31)
-            $0.trailing.equalTo(timeLabel)
+            $0.top.equalTo(memoLabel.snp.bottom).offset(16)
             $0.height.equalTo(28)
-            $0.bottom.equalToSuperview().offset(-20)
+            $0.leading.bottom.equalToSuperview()
         }
         
         myReactionBtn.snp.makeConstraints{
@@ -222,8 +233,16 @@ class FriendTableViewCell: BaseTableViewCell {
             $0.width.height.equalTo(myReactionBtn)
         }
         
-        theOtherReactionBtn.snp.makeConstraints{
-            $0.width.height.equalTo(myReactionBtn)
+        moreButton.snp.makeConstraints{
+            $0.trailing.equalToSuperview()
+            $0.centerY.equalTo(reactionStackView)
+            $0.width.equalTo(23)
+            $0.height.equalTo(22)
+        }
+        
+        separatorLine.snp.makeConstraints{
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(1)
         }
     }
 
