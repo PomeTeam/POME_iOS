c1bb2860c0e1581d06c80b9a27cadf072a0aebe9
Ssooya <bsy24660@naver.com>
2022-11-21 12:57:47 +0900

    #58: ConsumeReviewTableViewCell 파일 생성

---
+++ POME/domain/Cells/Review/ConsumeReviewTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ConsumeReviewTableViewCell.swift
+//  POME
+//
+//  Created by 박지윤 on 2022/11/21.
+//
+
+import UIKit
+
+class ConsumeReviewTableViewCell: UITableViewCell {
+    
+    static let cellIdentifier = "ConsumeReviewTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- POME/domain/ViewControllers/Review/ReviewViewController.swift
+++ POME/domain/ViewControllers/Review/ReviewViewController.swift
@@ -8,22 +8,71 @@
 import UIKit
 
 class ReviewViewController: BaseTabViewController {
+    
+    let mainView = ReviewView().then{
+        $0.backgroundColor = .red
+        $0.goalTagCollectionView.backgroundColor = .blue
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func style(){
+        
+        super.style()
     }
-    */
+    
+    override func layout(){
+        
+        super.layout()
+        
+        self.view.addSubview(mainView)
+        
+        mainView.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(Const.Offset.VIEW_CONTROLLER_TOP)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    override func initialize(){
+        mainView.goalTagCollectionView.delegate = self
+        mainView.goalTagCollectionView.dataSource = self
+        
+        mainView.consumeTableView.delegate = self
+        mainView.consumeTableView.dataSource = self
+    }
+}
 
+extension ReviewViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GoalCategoryCollectionViewCell.cellIdentifier, for: indexPath) as? GoalCategoryCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.setUnselectState()
+        
+        return cell
+                
+    }
+    
+    
+}
+
+extension ReviewViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ConsumeReviewTableViewCell.cellIdentifier, for: indexPath) as? ConsumeReviewTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    
 }
