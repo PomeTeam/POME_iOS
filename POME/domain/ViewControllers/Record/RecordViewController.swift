a332af8e1c8c7496d014ab28badea9fb3e5d5d4e
gom1n <alice436777@gmail.com>
2022-11-22 03:54:05 +0900

    #42: 기록카드 구현 및 바텀팝업창 연결

--- POME/domain/ViewControllers/Record/RecordViewController.swift
+++ POME/domain/ViewControllers/Record/RecordViewController.swift
@@ -42,7 +42,17 @@ class RecordViewController: BaseTabViewController {
         sheet.loadViewIfNeeded()
         self.present(sheet, animated: true, completion: nil)
     }
-    @objc func alertMenuButtonDidTap() {
+    @objc func cannotAddGoalButtonDidTap() {
+        let sheet = RecordBottomSheetViewController(Image.ten, "목표는 10개를 넘을 수 없어요", "포미는 사용자가 무리하지 않고 즐겁게 목표를\n달성할 수 있도록 응원하고 있어요!")
+        sheet.loadViewIfNeeded()
+        self.present(sheet, animated: true, completion: nil)
+    }
+    func cannotAddEmotionDidTap() {
+        let sheet = RecordBottomSheetViewController(Image.penPink, "아직은 감정을 기록할 수 없어요", "일주일이 지나야 감정을 남길 수 있어요\n나중에 다시 봐요!")
+        sheet.loadViewIfNeeded()
+        self.present(sheet, animated: true, completion: nil)
+    }
+    @objc func alertGoalMenuButtonDidTap() {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         let deleteAction =  UIAlertAction(title: "삭제하기", style: UIAlertAction.Style.default){(_) in
             let dialog = PopUpViewController(Image.trashCan, "목표를 삭제하시겠어요?", "해당 목표에서 작성한 기록도 모두 삭제돼요", "삭제할게요", "아니요")
@@ -51,6 +61,24 @@ class RecordViewController: BaseTabViewController {
         }
         let cancelAction = UIAlertAction(title: "취소", style: UIAlertAction.Style.cancel, handler: nil)
         
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true)
+    }
+    @objc func alertRecordMenuButtonDidTap() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let modifyAction =  UIAlertAction(title: "수정하기", style: UIAlertAction.Style.default){(_) in
+            print("click modify")
+        }
+        let deleteAction =  UIAlertAction(title: "삭제하기", style: UIAlertAction.Style.default){(_) in
+            let dialog = PopUpViewController(Image.trashCan, "기록을 삭제하시겠어요?", "삭제한 내용은 다시 되돌릴 수 없어요", "삭제할게요", "아니요")
+            dialog.modalPresentationStyle = .overFullScreen
+            self.present(dialog, animated: false, completion: nil)
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alert.addAction(modifyAction)
         alert.addAction(deleteAction)
         alert.addAction(cancelAction)
         
@@ -84,7 +112,7 @@ extension RecordViewController: UICollectionViewDelegate, UICollectionViewDataSo
 // MARK: - TableView delegate
 extension RecordViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 3 + 4
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let tag = indexPath.row
@@ -96,11 +124,14 @@ extension RecordViewController: UITableViewDelegate, UITableViewDataSource {
             cell.goalCollectionView.delegate = self
             cell.goalCollectionView.dataSource = self
             
+            // Add Goal
+            cell.goalPlusButton.addTarget(self, action: #selector(cannotAddGoalButtonDidTap), for: .touchUpInside)
+            
             return cell
         case 1:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "GoalTableViewCell", for: indexPath) as? GoalTableViewCell else { return UITableViewCell() }
             // Alert Menu
-            cell.menuButton.addTarget(self, action: #selector(alertMenuButtonDidTap), for: .touchUpInside)
+            cell.menuButton.addTarget(self, action: #selector(alertGoalMenuButtonDidTap), for: .touchUpInside)
             cell.selectionStyle = .none
             return cell
         case 2:
@@ -110,14 +141,17 @@ extension RecordViewController: UITableViewDelegate, UITableViewDataSource {
             return cell
         default:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecordCardTableViewCell", for: indexPath) as? RecordCardTableViewCell else { return UITableViewCell() }
-            
+            // Alert Menu
+            cell.menuButton.addTarget(self, action: #selector(alertRecordMenuButtonDidTap), for: .touchUpInside)
             cell.selectionStyle = .none
             return cell
         }
        
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        if indexPath.row > 2 {
+            cannotAddEmotionDidTap()
+        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
