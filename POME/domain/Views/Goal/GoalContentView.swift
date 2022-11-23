0f8d37dfbcdf5746d90843f0b401dcf21b62fc11
Ssooya <bsy24660@naver.com>
2022-11-23 14:28:02 +0900

    #77: 목표 추가 도메인 파일 추가

---
+++ POME/domain/Views/Goal/GoalContentView.swift
@@ -0,0 +1,20 @@
+//
+//  GoalContentView.swift
+//  POME
+//
+//  Created by 박지윤 on 2022/11/23.
+//
+
+import UIKit
+
+class GoalContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
