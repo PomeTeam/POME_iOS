cb450c9cc4db41acc92cd8dfe9c58f5ec7ce8c94
Ssooya <bsy24660@naver.com>
2022-12-17 20:15:10 +0900

    #85: CategorySheet UI 통한 카테고리 선택 기능 구현

--- POME/domain/ViewControllers/Record/Register/CategorySelectSheetViewController.swift
+++ POME/domain/ViewControllers/Record/Register/CategorySelectSheetViewController.swift
@@ -7,23 +7,76 @@
 
 import UIKit
 
-class CategorySelectSheetViewController: UIViewController {
+class CategorySelectSheetViewController: BaseSheetViewController {
+    
+    //MARK: - Properties
+    
+    var categorySelectHandler: ((String) -> ())!
+    
+    var categoryArray = ["커피","아이스크림","음료","생활","통신"]
+    
+    let mainView = CategorySelectSheetView().then{
+        $0.exitButton.addTarget(self, action: #selector(exitButtonDidClicked), for: .touchUpInside)
+    }
+    
+    //MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func style(){
+        
+        super.style()
+        
+        setBottomSheetStyle(type: .category)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func initialize(){
+        
+        super.initialize()
+        
+        mainView.categoryTableView.delegate = self
+        mainView.categoryTableView.dataSource = self
     }
-    */
+    
+    override func layout(){
+        
+        self.view.addSubview(mainView)
+        
+        mainView.snp.makeConstraints{
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+    
+    @objc func exitButtonDidClicked(){
+        self.dismiss(animated: true)
+    }
+}
 
+extension CategorySelectSheetViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categoryArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RecordCategoryTableViewCell.cellIdentifier, for: indexPath) as? RecordCategoryTableViewCell else { return UITableViewCell() }
+        
+        cell.nameLabel.text = categoryArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let selectedCell = tableView.cellForRow(at: indexPath) as? RecordCategoryTableViewCell,
+                let categoryTitle = selectedCell.nameLabel.text else { return }
+        
+        categorySelectHandler(categoryTitle)
+        self.dismiss(animated: true)
+    }
+    
 }
--- POME/global/Source/Sheet/SheetInfo.swift
+++ POME/global/Source/Sheet/SheetInfo.swift
@@ -11,4 +11,5 @@ enum SheetType: CGFloat{
     case friendReaction = 314 //314 + 34
     case emotionFilter = 218 //218+34
     case recordHome = 285
+    case category = 344 //top offset이 378임..
 }
