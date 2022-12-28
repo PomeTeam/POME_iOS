90627790afc25a1c183aa0274dd40dacb8614aee
Ssooya <bsy24660@naver.com>
2022-12-28 21:00:34 +0900

    refactor: DefaultTextField 코드 중복 리팩토링 및 countLimit 프로퍼티 추가

--- POME/global/Resource/Default/DefaultTextField.swift
+++ POME/global/Resource/Default/DefaultTextField.swift
@@ -10,17 +10,16 @@ import UIKit
 
 class DefaultTextField: UITextField {
     
+    var countLimit: Int?
+    
     init() {
+        
         super.init(frame: CGRect.zero)
-        
-        self.font = UIFont.autoPretendard(type: .r_14)
-        self.textColor = Color.title
-        
-        self.backgroundColor = Color.grey0
+
         self.clearButtonMode = .always
-        
-        self.layer.cornerRadius = 6
         self.addLeftPadding(16)
+        
+        initialize()
     }
     
     // 디폴트 textfield
@@ -28,32 +27,34 @@ class DefaultTextField: UITextField {
         super.init(frame: CGRect.zero)
         
         self.placeholder = placeholderStr
-        self.font = UIFont.autoPretendard(type: .r_14)
-        self.textColor = Color.title
-        
-        self.backgroundColor = Color.grey0
         self.clearButtonMode = .always
-        
-        self.layer.cornerRadius = 6
         self.addLeftPadding(16)
+        
+        initialize()
     }
     // padding 있는 textfield
     init(_ placeholderStr: String, _ leftPadding: CGFloat, _ rightPadding: CGFloat) {
         super.init(frame: CGRect.zero)
         
         self.placeholder = placeholderStr
-        self.font = UIFont.autoPretendard(type: .r_14)
-        self.textColor = Color.title
-        
-        self.backgroundColor = Color.grey0
 //        self.clearButtonMode = .always
-        
-        self.layer.cornerRadius = 6
+
         self.addLeftPadding(leftPadding)
         self.addRightPadding(rightPadding)
+        
+        initialize()
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initialize(){
+        
+        self.font = UIFont.autoPretendard(type: .r_14)
+        self.textColor = Color.title
+        self.backgroundColor = Color.grey0
+        
+        self.layer.cornerRadius = 6
     }
 }
