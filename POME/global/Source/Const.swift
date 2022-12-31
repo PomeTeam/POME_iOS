0f24005d86450e3f94055052e464cc13554b9b1a
Ssooya <bsy24660@naver.com>
2022-12-31 17:10:25 +0900

    refactor: 상태값 표현하는 정적 프로퍼티 가지는 ViewTag 구조체 선언 및 RecordRegisterEmotionSelectView에 적용

--- POME/global/Source/Const.swift
+++ POME/global/Source/Const.swift
@@ -8,14 +8,17 @@
 import Foundation
 import UIKit
 
-struct Const{
-    
-    struct Device{
-        static let WIDTH: CGFloat = UIScreen.main.bounds.size.width
-        static let HEIGHT: CGFloat = UIScreen.main.bounds.size.height
-    }
-    
-    struct Offset{
-        static let VIEW_CONTROLLER_TOP: CGFloat = 92
-    }
+
+struct Device{
+    static let WIDTH: CGFloat = UIScreen.main.bounds.size.width
+    static let HEIGHT: CGFloat = UIScreen.main.bounds.size.height
+}
+
+struct Offset{
+    static let VIEW_CONTROLLER_TOP: CGFloat = 92
+}
+
+struct ViewTag{
+    static let select = 1
+    static let deselect = 0
 }
--- POME/global/Source/Sheet/BaseSheetViewController.swift
+++ POME/global/Source/Sheet/BaseSheetViewController.swift
@@ -11,7 +11,7 @@ class BaseSheetViewController: UIViewController, UIViewControllerTransitioningDe
     
     //MARK: - Properties
     
-    private let type: SheetType!
+    private final let type: SheetType!
     
     init(type: SheetType){
         self.type = type
@@ -58,16 +58,12 @@ class BaseSheetViewController: UIViewController, UIViewControllerTransitioningDe
     
     func layout() {}
     
-//    func setBottomSheetStyle(type: SheetType){
-//        self.type = type
-//    }
-    
     private func getDetentSize() -> CGFloat{
         switch type{
         case .calendar:
             return type.rawValue + CalendarSheetCollectionViewCell.cellSize * 7
         case .category:
-            return Const.Device.HEIGHT - type.rawValue
+            return Device.HEIGHT - type.rawValue
         default:
             return type.rawValue
         }
