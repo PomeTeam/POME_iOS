b240b68fd53ece7b1ec5347cdaa4ff6326ee496b
gom1n <alice436777@gmail.com>
2023-01-17 23:05:17 +0900

    close #125: 온보딩 뷰 수정 - 소셜로그인 제거

--- POME/Presentation/Views/Login/LoginView.swift
+++ POME/Presentation/Views/Login/LoginView.swift
@@ -22,22 +22,12 @@ class LoginView: BaseView {
         $0.spacing = 15
         $0.axis = .horizontal
     }
-    let kakao = UIImageView().then{
-        $0.image = Image.kakao
-    }
-    let apple = UIImageView().then{
-        $0.image = Image.apple
-    }
     let startButton = DefaultButton(titleStr: "시작하기")
     
     override func hierarchy() {
         addSubview(logoImage)
         addSubview(logo)
         addSubview(slogan)
-        
-        addSubview(stack)
-        stack.addArrangedSubview(kakao)
-        stack.addArrangedSubview(apple)
         
         addSubview(startButton)
     }
@@ -59,20 +49,11 @@ class LoginView: BaseView {
             make.centerX.equalToSuperview()
             make.top.equalTo(logo.snp.bottom).offset(10)
         }
-        stack.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(slogan.snp.bottom).offset(50)
-        }
-        kakao.snp.makeConstraints { make in
-            make.width.height.equalTo(60)
-        }
-        apple.snp.makeConstraints { make in
-            make.width.height.equalTo(60)
-        }
         startButton.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(73)
             make.height.equalTo(45)
-            make.top.equalTo(stack.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(slogan.snp.bottom).offset(73)
         }
     }
 }
