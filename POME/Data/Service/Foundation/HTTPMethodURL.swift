fe63e021f6068fca9253d1971f73723d6feddf2e
SSOOYA <bsy24660@naver.com>
2023-01-25 13:54:07 +0900

    #113: HTTPMethodURL 설정

---
+++ POME/Data/Service/Foundation/HTTPMethodURL.swift
@@ -0,0 +1,62 @@
+//
+//  HTTPMethodURL.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/01/25.
+//
+
+import Foundation
+
+enum HTTPMethodURL {
+    
+    static let recordsURL = "/records"
+    static let goalsURL = "/goals"
+    static let userURL = "/users"
+    static let friendURL = "/users/friend"
+    static let goalCategoryURL = "/goal-category"
+    
+    struct GET {
+        static let records = HTTPMethodURL.recordsURL
+        static let recordOfGoal = HTTPMethodURL.recordsURL + "/goal" //기록 페이징 조회
+        
+        static let goal = HTTPMethodURL.goalsURL
+        static let pageOfGoalCategory = HTTPMethodURL.goalsURL + "/category"
+        
+        static let friendSearch = HTTPMethodURL.friendURL
+        static let friends = HTTPMethodURL.userURL + "/friends"
+        
+        static let goalCategory = HTTPMethodURL.goalCategoryURL
+    }
+    
+    struct POST {
+        static let record = HTTPMethodURL.recordsURL
+        static let secondEmotion = HTTPMethodURL.recordsURL
+        static let friendEmotion = HTTPMethodURL.recordsURL
+        
+        static let goal = HTTPMethodURL.goalsURL
+        
+        static let signUp = HTTPMethodURL.userURL + "/sign-up"
+        static let signIn = HTTPMethodURL.userURL + "/sign-in"
+        static let friend = HTTPMethodURL.friendURL
+        static let nicknameDuplicate = HTTPMethodURL.userURL + "/check-nickname"
+        static let sms = "/sms/send"
+        
+        static let goalCategory = HTTPMethodURL.goalCategoryURL
+        
+        static let authRenew = "/auth/renew"
+    }
+    
+    struct PUT {
+        static let record = HTTPMethodURL.recordsURL
+        static let goal = HTTPMethodURL.goalsURL
+    }
+    
+    struct DELETE {
+        static let record = HTTPMethodURL.recordsURL
+        static let goal = HTTPMethodURL.goalsURL
+        static let friend = HTTPMethodURL.friendURL
+        static let goalCategory = HTTPMethodURL.goalCategoryURL
+    }
+    
+    
+}
