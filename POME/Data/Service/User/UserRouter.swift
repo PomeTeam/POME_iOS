a5104492a55f652142eef70e65bfa5869c6e7111
gom1n <alice436777@gmail.com>
2023-01-25 23:37:30 +0900

    #135: 유저 관련 Router, Service 생성

--- POME/Data/Service/User/UserRouter.swift
+++ POME/Data/Service/User/UserRouter.swift
@@ -6,3 +6,79 @@
 //
 
 import Foundation
+import Moya
+
+enum UserRouter: BaseRouter{
+    case signUp(nickname: String, phoneNum: String, imageKey: String)
+    case signIn(phoneNum: String)
+    case postFriend(friendId: Int)
+    case deleteFriend(friendId: Int)
+    case getFriendSearch(friendId: String, pageable: PageableModel)
+    case getFriends(pageable: PageableModel)
+    case checkNickname(nickName: String)
+}
+
+extension UserRouter{
+    
+    var path: String {
+        switch self {
+        case .signUp(let nickname, let phoneNum, let imageKey):
+            return HTTPMethodURL.POST.signUp
+        case .signIn(let phoneNum):
+            return HTTPMethodURL.POST.signIn
+        case .getFriendSearch(let id):
+            return HTTPMethodURL.GET.friendSearch + "/\(id)"
+        case .postFriend(let id):
+            return HTTPMethodURL.POST.friend + "/\(id)"
+        case .deleteFriend(let id):
+            return HTTPMethodURL.DELETE.friend + "/\(id)"
+        case .getFriends:
+            return HTTPMethodURL.GET.friends
+        case .checkNickname(let nickName):
+            return HTTPMethodURL.POST.nicknameDuplicate
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .signUp:
+            return .post
+        case .signIn:
+            return .post
+        case .getFriendSearch:
+            return .get
+        case .postFriend:
+            return .post
+        case .deleteFriend:
+            return .delete
+        case .getFriends:
+            return .get
+        case .checkNickname:
+            return .post
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .signUp(let nickname, let phoneNum, let imageKey):
+            return .requestParameters(parameters: ["nickname": nickname,
+                                                   "phoneNum": phoneNum,
+                                                   "imageKey": imageKey],
+                                      encoding: JSONEncoding.default)
+        case .signIn(let phoneNum):
+            return .requestParameters(parameters: ["phoneNum": phoneNum],
+                                      encoding: JSONEncoding.default)
+        case .getFriendSearch:
+            return .requestPlain
+        case .postFriend:
+            return .requestPlain
+        case .deleteFriend:
+            return .requestPlain
+        case .getFriends(let pageable):
+            return .requestParameters(parameters: ["userId": UserManager.userId ?? "",
+                                                   "pageable" : pageable], encoding: URLEncoding.queryString)
+        case .checkNickname(let nickName):
+            return .requestParameters(parameters: ["nickName": nickName], encoding: URLEncoding.queryString)
+        }
+    }
+}
--- POME/Data/Service/User/UserService.swift
+++ POME/Data/Service/User/UserService.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+final class UserService: MultiMoyaService{
+    static let shared = UserService()
+    private init() { }
+}
+
+extension UserService{
+    
+    func signUp(nickname: String, phoneNum: String, imageKey: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        requestNoResultAPI(UserRouter.signUp(nickname: nickname, phoneNum: phoneNum, imageKey: imageKey)){ response in
+            completion(response)
+        }
+    }
+    
+    func signIn(phoneNum: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        requestNoResultAPI(UserRouter.signIn(phoneNum: phoneNum)){ response in
+            completion(response)
+        }
+    }
+    
+    func getFriendSearch(id: Int, completion: @escaping (Result<Int, Error>) -> Void) {
+        requestNoResultAPI(FriendRouter.getFriendSearch(id: id)){ response in
+            completion(response)
+        }
+    }
+    
+    func generateNewFriend(id: Int, completion: @escaping (Result<Int, Error>) -> Void) {
+        requestNoResultAPI(FriendRouter.postFriend(id: id)){ response in
+            completion(response)
+        }
+    }
+    
+    func deleteFriend(id: Int, completion: @escaping (Result<Int, Error>) -> Void) {
+        requestNoResultAPI(FriendRouter.deleteFriend(id: id)){ response in
+            completion(response)
+        }
+    }
+    
+    func getFriends(pageable: PageableModel, completion: @escaping (Result<[FriendsResponseModel], Error>) -> Void) {
+        requestDecoded(FriendRouter.getFriends(pageable: pageable)){ response in
+            completion(response)
+        }
+    }
+    
+    func checkNickname(nickName: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        requestNoResultAPI(UserRouter.checkNickname(nickName: nickName)){ response in
+            completion(response)
+        }
+    }
+}
