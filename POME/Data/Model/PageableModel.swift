aa87f7f19968ec15d6b5cdec6f8f050db63fe18b
SSOOYA <bsy24660@naver.com>
2023-01-25 15:33:37 +0900

    #113: BaseRequestModel 및 PageableModel 추가

---
+++ POME/Data/Model/PageableModel.swift
@@ -0,0 +1,14 @@
+//
+//  PageableModel.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/01/25.
+//
+
+import Foundation
+
+struct PageableModel: Encodable{
+    let page: Int
+    let size: Int
+    let sort: String
+}
