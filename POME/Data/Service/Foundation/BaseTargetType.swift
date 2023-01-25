f2115ab2002bd2c453ff898064128f8556425ef6
SSOOYA <bsy24660@naver.com>
2023-01-25 13:53:45 +0900

    #113: BaseTargetType 설정

---
+++ POME/Data/Service/Foundation/BaseTargetType.swift
@@ -0,0 +1,34 @@
+//
+//  BaseTargetType.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/01/25.
+//
+
+import Foundation
+import Moya
+
+protocol BaseTargetType: Moya.TargetType {
+    associatedtype ResultModel: Decodable
+}
+
+extension BaseTargetType {
+
+    var baseURL: URL {
+        let url = Bundle.main.infoDictionary?["API_URL"] as? String ?? ""
+        return URL(string: "http://" + url)!
+    }
+
+    var headers: [String: String]? {
+        let token = UserManager.token ?? ""
+        let header = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token]
+        return header
+    }
+
+    var sampleData: Data {
+        return Data()
+    }
+
+}
