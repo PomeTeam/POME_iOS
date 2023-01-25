87e77b7d154b730964b161df5e9cfc1852d56a6d
SSOOYA <bsy24660@naver.com>
2023-01-25 13:54:33 +0900

    #113: MultiMoyaProvider 설정

---
+++ POME/Data/Service/Foundation/MultiMoyaProvider.swift
@@ -0,0 +1,95 @@
+//
+//  MultiMoyaProvider.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/01/25.
+//
+
+import Foundation
+import Moya
+
+final class MultiMoyaProvider: MoyaProvider<MultiTarget> {
+    
+    var request: Cancellable?
+    
+    func requestDecoded<T: BaseTargetType>(_ target: T,
+                                           completion: @escaping (Result<T.ResultModel?, Error>) -> Void) {
+        addObserver()
+        request(MultiTarget(target)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let body = try JSONDecoder().decode(T.ResultModel.self, from: response.data)
+                    completion(.success(body))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func requestNoResultAPI<T: BaseTargetType>(_ target: T,
+                                               completion: @escaping (Result<Int?, Error>) -> Void) {
+        addObserver()
+        request = request(MultiTarget(target)) { result in
+            switch result {
+            case .success(let response):
+                completion(.success(response.statusCode))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func requestWithProgress<T: BaseTargetType>(_ target: T,
+                                                progressCompletion: @escaping ((ProgressResponse) -> Void),
+                                                completion: @escaping (Result<Int?, Error>) -> Void) {
+        addObserver()
+        request = request(MultiTarget(target)) { progress in
+            progressCompletion(progress)
+        } completion: { result in
+            switch result {
+            case .success(let response):
+                completion(.success(response.statusCode))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func requestDecodedMultiRepsonse<T: BaseTargetType, R: Decodable>(_ target: T,
+                                                                      _ requestModel: R.Type,
+                                                                      completion: @escaping (Result<R?, Error>) -> Void) {
+        addObserver()
+        request = request(MultiTarget(target)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let body = try JSONDecoder().decode(R.self, from: response.data)
+                    completion(.success(body))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func addObserver() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(cancelRequest),
+                                               name: Notification.Name("requestCancel"),
+                                               object: nil)
+    }
+    
+    @objc func cancelRequest(notification: NSNotification) {
+        guard let request = request else {
+            return
+        }
+        request.cancel()
+    }
+    
+}
---
+++ POME/Global/Source/UserManager/UserManager.swift
@@ -0,0 +1,16 @@
+//
+//  UserManager.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/01/25.
+//
+
+import Foundation
+
+class UserManager {
+    @UserDefault(key: UserDefaultKey.id, defaultValue: nil)
+    static var userId: String?
+    
+    @UserDefault(key: UserDefaultKey.token, defaultValue: nil)
+    static var token: String?
+}
