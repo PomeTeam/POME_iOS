96c9a94feee4a6291b2068e91bdc64f6c7351dd4
SSOOYA <bsy24660@naver.com>
2023-01-26 14:30:26 +0900

    #139: Goal 도메인 Router, Service 설계

--- POME/Data/Service/Goal/GoalRouter.swift
+++ POME/Data/Service/Goal/GoalRouter.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import Moya
+
+enum GoalRouter: BaseRouter{
+    case postGoal(request: GoalRegisterRequestModel)
+    case getGoal(id: Int)
+    case putGoal(id: Int, request: GoalRegisterRequestModel)
+    case deleteGoal(id: Int)
+    case getGoalsByCategory(id: Int, pageable: PageableModel)
+}
+
+extension GoalRouter{
+    var path: String {
+        switch self {
+        case .postGoal:
+            return HTTPMethodURL.POST.goal
+        case .getGoal(let id):
+            return HTTPMethodURL.GET.goal + "/\(id)"
+        case .putGoal(let id, _):
+            return HTTPMethodURL.PUT.goal + "/\(id)"
+        case .deleteGoal(let id):
+            return HTTPMethodURL.DELETE.goal +  "/\(id)"
+        case .getGoalsByCategory(let id, _):
+            return HTTPMethodURL.GET.pageOfGoalCategory + "/\(id)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self{
+        case .postGoal:
+            return .post
+        case .getGoal:
+            return .get
+        case .putGoal:
+            return .put
+        case .deleteGoal:
+            return .delete
+        case .getGoalsByCategory:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self{
+        case .postGoal(let request):
+            return .requestPlain
+        case .getGoal:
+            return .requestPlain
+        case .putGoal(_, let request):
+            return .requestPlain
+        case .deleteGoal:
+            return .requestPlain
+        case .getGoalsByCategory(let pageable):
+            return .requestPlain
+        }
+    }
+}
