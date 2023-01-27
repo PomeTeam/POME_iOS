39a58033375479a8cca8bd1941fe5712f6ebbe2c
SSOOYA <bsy24660@naver.com>
2023-01-27 12:53:37 +0900

    #143: RecordResponseModel 감정 선택안한 경우 위한 옵셔널 타입 선언 및 옵셔널인 경우 데이터 바인딩 처리 구현

--- POME/Data/Model/Record/RecordResponseModel.swift
+++ POME/Data/Model/Record/RecordResponseModel.swift
@@ -19,23 +19,36 @@ struct RecordResponseModel: Decodable{
 
 struct EmotionResponseModel: Decodable{
     let firstEmotion: Int
-    let secondEmotion: Int
-    var myEmotion: Int
+    let secondEmotion: Int?
+    var myEmotion: Int?
     let friendEmotions: [Int]
 }
 
 
 extension RecordResponseModel{
     
+    //TODO: 감정/리액션 없을 때 null
+    
     var firstEmotionBinding: EmotionTag{
-        EmotionTag(rawValue: self.emotionResponse.firstEmotion) ?? .happy
+        EmotionTag(rawValue: self.emotionResponse.firstEmotion) ?? .default
     }
     
     var secondEmotionBinding: EmotionTag{
-        EmotionTag(rawValue: self.emotionResponse.secondEmotion) ?? .happy
+        EmotionTag(rawValue: self.emotionResponse.secondEmotion ?? Const.default) ?? .default
     }
     
-    var myReactionBinding: Reaction{
-        Reaction(rawValue: self.emotionResponse.myEmotion) ?? .happy
+    var myReactionBinding: UIImage{
+        guard let reaction = self.emotionResponse.myEmotion else {
+            return Image.emojiAdd
+        }
+        return Reaction(rawValue: reaction)?.defaultImage ?? Image.emojiAdd
+    }
+    
+    var othersThumbnailReaction: Reaction{
+        Reaction(rawValue: self.emotionResponse.friendEmotions.first!) ?? .happy
+    }
+    
+    var othersReactionCount: Int{
+        self.emotionResponse.friendEmotions.count
     }
 }
