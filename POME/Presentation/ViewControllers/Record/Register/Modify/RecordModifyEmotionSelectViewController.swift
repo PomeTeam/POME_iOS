3d4f8e1b5a0db5e0e199f6883f55b38ef26a016a
SSOOYA <bsy24660@naver.com>
2023-02-05 12:48:56 +0900

    #182: 기록 수정 UI 파일 추가

---
+++ POME/Presentation/ViewControllers/Record/Register/Modify/RecordModifyEmotionSelectViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RecordModifyEmotionSelectViewController.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/02/05.
+//
+
+import Foundation
+
+class RecordModifyEmotionSelectViewController: RecordRegisterEmotionSelectViewController{
+    
+}
