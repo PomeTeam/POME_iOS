2a3aed4c03038fc28a0e96b8a72288f403317ac6
SSOOYA <bsy24660@naver.com>
2023-02-08 15:54:55 +0900

    #202: 친구 닉네임 - 이미지 Hash 데이터 관리 위한 싱글톤 클래스 FriendProfileImageManager 생성

---
+++ POME/Presentation/Utils/Friend/FriendProfileImageManager.swift
@@ -0,0 +1,15 @@
+//
+//  FriendProfileImageManager.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/02/08.
+//
+
+import Foundation
+
+class FriendProfileImageManager{
+    
+    static let shared = FriendProfileImageManager()
+    
+    private init() {}
+}
