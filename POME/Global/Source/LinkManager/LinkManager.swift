c69a69f0944759bf8d1f3c78361e321e2170a458
gom1n <alice436777@gmail.com>
2023-02-11 20:56:03 +0900

    #187: 노션 페이지 랜딩 (오픈소스 라이센스 제외)

--- POME/Global/Source/LinkManager/LinkManager.swift
+++ POME/Global/Source/LinkManager/LinkManager.swift
@@ -10,12 +10,23 @@ import UIKit
 import SafariServices
 
 enum Link: String {
-    case serviceUse = "https://few-horse-2aa.notion.site/0bb91905ab734733a8baddfa5d459e07"
-    case privacyTerm = "https://few-horse-2aa.notion.site/8d8b53bcae774895bb080abaa9b15319"
-    case privacyUse = "https://few-horse-2aa.notion.site/1f71b0a5adac4b1ea1d8d7d0c1405ccc"
-    case marketingTerm = "https://few-horse-2aa.notion.site/695cc52385ed427591768bb20b2053af"
+    // 개인정보 처리방침 및 서비스 이용약관
+    case privacyAndServiceUse = "https://few-horse-2aa.notion.site/6a2e6f6241e94a479a3e2c2a3cdb909e"
+    // 서비스 이용 약관
+    case serviceUse = "https://few-horse-2aa.notion.site/c646f930ac5a40a1b519471b940d203a"
+    // 개인 정보 수집 동의
+    case privacyTerm = "https://few-horse-2aa.notion.site/b396b02d8bd3460f945cf3f90935667b"
+    // 마케팅 정보 활용 동의
+    case marketingTerm = "https://few-horse-2aa.notion.site/8cc486236d77473f84fa53a3b0ede726"
+    // 문의 양식
     case contact = "https://forms.gle/SaFj5Mi9Gq1yLGff9"
+    // 신고 양식
     case report = "https://forms.gle/NhQ55kbYdkxnNseq7"
+    // 인증번호 오류
+    case codeError = "https://few-horse-2aa.notion.site/3b12466e1ff14690b5c2d05c273fb2cf"
+    
+    //TODO: 오픈소스 라이센스
+    case openSourceLicense = ""
 }
 
 class LinkManager {
@@ -29,6 +40,7 @@ class LinkManager {
         self.viewcontroller?.present(linkView, animated: true, completion: nil)
     }
     
+    // urlStr로 이동하는 메서드
     func linkTo(_ urlStr: String) {
         guard let url = NSURL(string: urlStr) else {return}
         let linkView: SFSafariViewController = SFSafariViewController(url: url as URL)
