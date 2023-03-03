80fb231e2b87d75b8ab50798617857b437ba5238
SSOOYA <bsy24660@naver.com>
2023-03-03 20:49:04 +0900

    #254: SceneDelegate 회원정보 유무에 따른 시작 화면 분기

--- POME/AppDelegate/AppDelegate.swift
+++ POME/AppDelegate/AppDelegate.swift
@@ -10,12 +10,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // MARK: LaunchScreen
         sleep(2)
-        
         
         return true
     }
