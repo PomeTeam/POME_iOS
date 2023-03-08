ecbf8bdf0d662d421c20a687d9cf0156cc986978
SSOOYA <bsy24660@naver.com>
2023-03-08 20:55:42 +0900

    #269: 회고탭 목표 선택시 UI 상 중복 선택으로 보이는 버그 해결

--- POME/Presentation/ViewControllers/Review/ReviewViewController.swift
+++ POME/Presentation/ViewControllers/Review/ReviewViewController.swift
@@ -188,20 +188,21 @@ extension ReviewViewController: UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         goals.isEmpty ? GoalTagCollectionViewCell.estimatedSize() : GoalTagCollectionViewCell.estimatedSize(title: goals[indexPath.row].name)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if let willDeselctCell = collectionView.cellForItem(at: [0, currentGoal]) as? GoalTagCollectionViewCell{
+            willDeselctCell.setUnselectState(with: goals[currentGoal].isGoalEnd)
+        }
+        
         guard let cell = collectionView.cellForItem(at: indexPath) as? GoalTagCollectionViewCell else { return }
         if(currentGoal == 0 && indexPath.row != 0){
-            guard let cell = collectionView.cellForItem(at: [0,0]) as? GoalTagCollectionViewCell else { return }
-            cell.setUnselectState(with: goals[indexPath.row].isGoalEnd)
+            guard let initialCell = collectionView.cellForItem(at: [0,0]) as? GoalTagCollectionViewCell else { return }
+            initialCell.setUnselectState(with: goals[0].isGoalEnd)
         }
+
         currentGoal = indexPath.row
         cell.setSelectState()
-    }
-
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? GoalTagCollectionViewCell else { return }
-        cell.setUnselectState(with: goals[indexPath.row].isGoalEnd)
     }
 }
 
