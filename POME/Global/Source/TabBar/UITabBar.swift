46459dfd8bd2d8c21a21c1469a9ca45858c15a44
SSOOYA <bsy24660@naver.com>
2023-03-12 21:57:55 +0900

    #273: UITabBar shadowRadius 수치 변경

--- POME/Global/Source/TabBar/UITabBar.swift
+++ POME/Global/Source/TabBar/UITabBar.swift
@@ -20,7 +20,7 @@ extension CALayer {
         shadowColor = color.cgColor
         shadowOpacity = alpha
         shadowOffset = CGSize(width: x, height: y)
-        shadowRadius = blur
+        shadowRadius = blur / 2
     }
 }
 
