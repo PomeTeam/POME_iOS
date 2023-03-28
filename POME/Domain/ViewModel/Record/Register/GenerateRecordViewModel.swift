83aef83ac9541156ddd7704ae0713582810a354e
SSOOYA <bsy24660@naver.com>
2023-03-28 09:40:48 +0900

    #287: 기록 생성/수정 ViewModel 재설계 위한 파일 생성

---
+++ POME/Domain/ViewModel/Record/Register/GenerateRecordViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GenerateRecordViewModel.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/03/27.
+//
+
+import Foundation
---
+++ POME/Domain/ViewModel/Record/Register/ModifyRecordViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ModifyRecordViewModel.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/03/27.
+//
+
+import Foundation
