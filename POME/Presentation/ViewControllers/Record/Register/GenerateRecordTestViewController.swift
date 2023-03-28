07ab0e75f09cbe66c40b42363a51be0c353e8713
SSOOYA <bsy24660@naver.com>
2023-03-28 09:40:19 +0900

    #286: 기록 생성/수정 클래스 재설계 테스트 위한 파일 생성

---
+++ POME/Presentation/ViewControllers/Record/Register/GenerateRecordTestViewController.swift
@@ -0,0 +1,12 @@
+//
+//  GenerateRecordTestViewController.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/03/27.
+//
+
+import Foundation
+
+class GenerateRecordTestViewController: Recordable{
+    
+}
---
+++ POME/Presentation/ViewControllers/Record/Register/ModifyRecordTestViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ModifyRecordTestViewController.swift
+//  POME
+//
+//  Created by 박소윤 on 2023/03/27.
+//
+
+import Foundation
+
+class ModifyRecordTestViewController: Recordable{
+    
+}
