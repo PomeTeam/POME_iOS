279fba619598f4448f02417039ba0c4501e8fdea
SSOOYA <bsy24660@naver.com>
2023-03-28 16:20:44 +0900

    #286: 기록 생성/수정 공통 클래스 Recordable 설계

--- POME/Presentation/ViewControllers/Record/Register/Recordable.swift
+++ POME/Presentation/ViewControllers/Record/Register/Recordable.swift
@@ -6,29 +6,53 @@
 //
 
 import Foundation
-
-//ViewModel 따로 만들건지 재사용할건지..? 재사용한다면 완료 버튼만 다른데 어떻게 구현할 건지..?
-protocol RecordableViewModel{
-    
-}
+import RxSwift
 
 class Recordable: BaseViewController{
     
-    enum RecordType: String{
-        case generate = "작성했어요"
-        case modify = "수정했어요"
+    @frozen
+    enum RecordType{
+        case generate
+        case modify
     }
     
     let mainView: RecordContentView
-//    let viewModel: RecordableViewModel
+    let viewModel: RecordableViewModel
     
-    init(recordType: RecordType, mainView: RecordContentView){
-        self.mainView = mainView
+    init(recordType: RecordType, viewModel: RecordableViewModel){
+        self.mainView = recordType.mainView
+        self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
-        mainView.completeButton.setTitle(recordType.rawValue, for: .normal)
+        mainView.completeButton.setTitle(recordType.completeButtonTitle, for: .normal)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layout() {
+        super.layout()
+        view.addSubview(mainView)
+        mainView.snp.makeConstraints{
+            $0.top.equalTo(navigationView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
+
+extension Recordable.RecordType{
+    
+    var completeButtonTitle: String{
+        switch self{
+        case .generate:     return "작성했어요"
+        case .modify:       return "수정했어요"
+        }
+    }
+    
+    var mainView: RecordContentView{
+        switch self{
+        case .generate:     return RecordRegisterContentView()
+        case .modify:       return RecordContentView()
+        }
     }
 }
