ee2f92c72457c610f52050a5eeefd54a6988fd65
SSOOYA <bsy24660@naver.com>
2023-03-30 19:43:14 +0900

    #287: ModifyRecordUseCase 설계 및 프로토콜 통한 클래스 구현

--- POME/Domain/UseCase/Record/ModifyRecordUseCase.swift
+++ POME/Domain/UseCase/Record/ModifyRecordUseCase.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+
+struct ModifyRecordRequestValue{
+    let id: Int
+    let recordInfo: RecordDTO
+}
+
+protocol ModifyRecordUseCaseInterface {
+    func execute(requestValue: ModifyRecordRequestValue) -> Observable<Int>
+}
+
+final class ModifyRecordUseCase: ModifyRecordUseCaseInterface {
+
+    private let recordRepository: RecordRepositoryInterface
+
+    init(recordRepository: RecordRepositoryInterface = RecordRepository()) {
+        self.recordRepository = recordRepository
+    }
+
+    func execute(requestValue: ModifyRecordRequestValue) -> Observable<Int>{
+        return recordRepository.modifyRecord(requestValue: requestValue)
+    }
+}
