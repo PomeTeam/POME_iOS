eb104db93e1cd1b029d7f333d297fddf0711706a
SSOOYA <bsy24660@naver.com>
2023-04-02 10:59:28 +0900

    #293: GenerateGoalContentVC ViewModel 재설계 버전 적용

--- POME/Presentation/ViewControllers/Goal/GenerateGoalContentViewController.swift
+++ POME/Presentation/ViewControllers/Goal/GenerateGoalContentViewController.swift
@@ -8,17 +8,16 @@
 import UIKit
 import RxSwift
 
+enum GenerateGoalStatus: String{
+    case success
+    case duplicateGoal = "G0004" //Goal 생성 시, 이미 등록된 Goal-Category 명으로 등록한 경우
+}
+
 class GenerateGoalContentViewController: BaseViewController {
-    
-    enum GoalError: String{
-        case duplicateGoal = "G0004" //Goal 생성 시, 이미 등록된 Goal-Category 명으로 등록한 경우
-        case `default`
-    }
     
     private let mainView = GoalContentView()
     private let viewModel = GenerateGoalContentViewModel()
     private let goalDateRange: GoalDateDTO
-    private var goalDataManager = GoalRegisterRequestManager.shared
     
     //MARK: - Override
     
@@ -37,66 +36,61 @@ class GenerateGoalContentViewController: BaseViewController {
     }
     
     override func layout(){
-        
         super.layout()
-        
-        self.view.addSubview(mainView)
+        view.addSubview(mainView)
         mainView.snp.makeConstraints{
             $0.top.equalTo(navigationView.snp.bottom)
             $0.leading.trailing.bottom.equalToSuperview()
         }
     }
     
-    override func initialize(){
-        
-        super.initialize()
-        
-        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(keyboardWillDisappear)))
-        
-        mainView.categoryField.infoTextField.delegate = self
-        mainView.promiseField.infoTextField.delegate = self
-        mainView.priceField.infoTextField.delegate = self
-    }
-    
     override func bind(){
 
-        let input = GenerateGoalContentViewModel.Input(categoryText: mainView.categoryField.infoTextField.rx.text.orEmpty.asObservable(),
-                                                       promiseText: mainView.promiseField.infoTextField.rx.text.orEmpty.asObservable(),
-                                                       priceText: mainView.priceField.infoTextField.rx.text.orEmpty.asObservable())
+        let input = GenerateGoalContentViewModel.Input(dateRange: goalDateRange,
+                                                       goal: mainView.categoryField.infoTextField.rx.text.orEmpty.asObservable(),
+                                                       oneLineMind: mainView.promiseField.infoTextField.rx.text.orEmpty.asObservable(),
+                                                       price: mainView.priceField.infoTextField.rx.text.orEmpty.asObservable(),
+                                                       isPublic: mainView.goalMakePublicSwitch.rx.isOn.asObservable().startWith(true),
+                                                       ctaButtonTap: mainView.completeButton.rx.tap)
         
         let output = viewModel.transform(input: input)
         
-        output.categoryText
-            .drive(onNext: {
-                self.goalDataManager.name = $0
-            }).disposed(by: disposeBag)
+        output.categoryBinding
+            .drive(mainView.categoryField.infoTextField.rx.text)
+            .disposed(by: disposeBag)
         
-        output.promiseText
-            .drive(onNext: {
-                self.goalDataManager.oneLineMind = $0
-            }).disposed(by: disposeBag)
+        output.oneLineMindBinding
+            .drive(mainView.promiseField.infoTextField.rx.text)
+            .disposed(by: disposeBag)
 
-        output.priceText
-            .drive(onNext: {
-                self.goalDataManager.price = $0
+        output.priceBinding
+            .drive(mainView.priceField.infoTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.isPublicBinding
+            .drive(onNext: { [weak self] value in
+                self?.mainView.changeGoalMakePublicViewStatus(with: value)
             }).disposed(by: disposeBag)
         
-        output.canMoveNext
+        output.ctaButtonActivate
             .drive(mainView.completeButton.rx.isActivate)
             .disposed(by: disposeBag)
         
-        mainView.completeButton.rx.tap
-            .bind{
-                self.requestGenerateGoal()
-            }.disposed(by: disposeBag)
+        output.generateGoalStatusCode
+            .subscribe(onNext: { [weak self] statusCode in
+                switch statusCode {
+                case .success:
+                    self?.processResponseGenerateGoal()
+                case .duplicateGoal:
+                    self?.processResponseDuplicateGoal()
+                }
+            }).disposed(by: disposeBag)
         
-        mainView.goalMakePublicSwitch.rx.isOn
-            .asDriver(onErrorJustReturn: false)
-            .drive(onNext: { value in
-                self.goalDataManager.isPublic = value
-                self.mainView.changeGoalMakePublicViewStatus(with: value)
-            })
-            .disposed(by: disposeBag)
+        view.rx.tapGesture()
+            .when(.recognized)
+            .bind(onNext: { [weak self] _ in
+                self?.view.endEditing(true)
+            }).disposed(by: disposeBag)
         
         etcButton.rx.tap
             .bind{
@@ -107,129 +101,16 @@ class GenerateGoalContentViewController: BaseViewController {
     //MARK: - Helper
     
     private func closeButtonDidClicked(){
-        let dialog = ImageAlert.quitRecord.generateAndShow(in: self)
-        dialog.completion = {
-            self.navigationController?.popToRootViewController(animated: true)
-        }
-    }
-    
-    @objc func keyboardWillDisappear(){
-        self.view.endEditing(true)
-    }
-}
-
-//MARK: - UITextFieldDelegate
-
-extension GenerateGoalContentViewController: UITextFieldDelegate{
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.setFocusState()
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        textField.setUnfocusState()
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool{
-        
-        guard let textField = textField as? DefaultTextField else { return false }
-        
-        if(textField == mainView.priceField.infoTextField){
-            
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .decimal // 1,000,000
-            formatter.locale = Locale.current
-            formatter.maximumFractionDigits = 0 // 허용하는 소숫점 자리수
-
-            if let removeAllSeprator = textField.text?.replacingOccurrences(of: formatter.groupingSeparator, with: ""){
-                var beforeForemattedString = removeAllSeprator + string
-                if formatter.number(from: string) != nil {
-                    if let formattedNumber = formatter.number(from: beforeForemattedString), let formattedString = formatter.string(from: formattedNumber){
-                        textField.text = formattedString
-                        return false
-                    }
-                }else{ // 숫자가 아닐 때먽
-                    if string == "" { // 백스페이스일때
-                        let lastIndex = beforeForemattedString.index(beforeForemattedString.endIndex, offsetBy: -1)
-                        beforeForemattedString = String(beforeForemattedString[..<lastIndex])
-                        if let formattedNumber = formatter.number(from: beforeForemattedString), let formattedString = formatter.string(from: formattedNumber){
-                            textField.text = formattedString
-                            return false
-                        }
-                    }else{ // 문자일 때
-                        return false
-                    }
-                }
-
-            }
-            
-            return true
-        }
-        
-        guard let oldString = textField.text, let textCountLimit = textField.countLimit else { return true }
-        
-        if(oldString.count < textCountLimit){
-            return true
-        }
-        
-        guard let changedRange = Range(range, in: oldString) else { return false}
-        let newString = oldString.replacingCharacters(in: changedRange, with: string)
-        
-        return newString.count <= textCountLimit
-    }
-}
-
-
-//MARK: - API
-extension GenerateGoalContentViewController{
-    
-    private func requestGenerateGoal(){
-
-        guard let price = Int(goalDataManager.price) else { return }
-        let request = GoalRegisterRequestModel(name: goalDataManager.name,
-                                               startDate: goalDataManager.startDate,
-                                               endDate: goalDataManager.endDate,
-                                               oneLineMind: goalDataManager.oneLineMind,
-                                               price: price,
-                                               isPublic: goalDataManager.isPublic)
-        print(request)
-        print("LOG: requestData requestGenerateGoal", request)
-        
-        GoalService.shared.generateGoal(request: request){ result in
-            switch result{
-            case .success:
-                print("LOG: success requestGenerateGoal")
-                self.processResponseGenerateGoal()
-                return
-            case.invalidSuccess(let code, let message):
-                print("LOG: invalidSuccess requestGenerateGoal", message)
-                self.checkErrorCode(code){ error in
-                    switch error{
-                    case .duplicateGoal:
-                        self.processResponseDuplicateGoal()
-                        return
-                    default:
-                        break
-                    }
-                }
-            default:
-                print(result)
-                NetworkAlert.show(in: self){ [weak self] in
-                    self?.requestGenerateGoal()
-                }
-                break
+        ImageAlert.quitRecord.generateAndShow(in: self).do{
+            $0.completion = {
+                self.navigationController?.popToRootViewController(animated: true)
             }
         }
     }
     
     private func processResponseGenerateGoal(){
         let vc = RegisterSuccessViewController(type: .goal)
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    private func checkErrorCode(_ code: String, closure: (GoalError) -> Void){
-        let error = GoalError(rawValue: code) ?? .default
-        closure(error)
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     private func processResponseDuplicateGoal(){
