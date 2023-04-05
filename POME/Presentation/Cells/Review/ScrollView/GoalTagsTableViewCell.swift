62be4360556ab15d7e886c0b665abbadf1acd372
SSOOYA <bsy24660@naver.com>
2023-04-05 19:50:07 +0900

    #295: GoalTagsTableViewCell cell register 메서드 cellType 매개변수 시그니처로 변경

--- POME/Presentation/Cells/Review/ScrollView/GoalTagsTableViewCell.swift
+++ POME/Presentation/Cells/Review/ScrollView/GoalTagsTableViewCell.swift
@@ -21,7 +21,7 @@ class GoalTagsTableViewCell: BaseTableViewCell{
         $0.showsHorizontalScrollIndicator = false
         $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         
-        $0.register(GoalTagCollectionViewCell.self, forCellWithReuseIdentifier: GoalTagCollectionViewCell.cellIdentifier)
+        $0.register(cellType:GoalTagCollectionViewCell.self)
     }
     
     override func hierarchy() {
