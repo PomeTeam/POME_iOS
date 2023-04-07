436a6becd27c6e10e6353df2a1665b30644e5279
SSOOYA <bsy24660@naver.com>
2023-04-07 20:27:27 +0900

    #295: 회고탭 기록 조회 Repository, Usecase 설계 및 메서드 기능 구현

--- POME/Domain/UseCase/Record/GetRecordInReviewUseCase.swift
+++ POME/Domain/UseCase/Record/GetRecordInReviewUseCase.swift
@@ -1,8 +1,33 @@
 //
-//  GetRecordUseCase.swift
+//  GetRecordInReviewUseCase.swift
 //  POME
 //
 //  Created by 박소윤 on 2023/03/28.
 //
 
 import Foundation
+import RxSwift
+
+struct GetRecordInReviewRequestModel{
+    let firstEmotion: Int?
+    let secondEmotion: Int?
+    let pageable: PageableModel
+}
+
+protocol GetRecordInReviewUseCaseInterface {
+    func execute(goalId: Int, requestValue: GetRecordInReviewRequestModel) -> Observable<PageableResponseModel<RecordResponseModel>>
+}
+
+final class GetRecordInReviewUseCase: GetRecordInReviewUseCaseInterface {
+
+    private let recordRepository: RecordRepositoryInterface
+
+    init(recordRepository: RecordRepositoryInterface = RecordRepository()) {
+        self.recordRepository = recordRepository
+    }
+
+    func execute(goalId: Int, requestValue: GetRecordInReviewRequestModel) -> Observable<PageableResponseModel<RecordResponseModel>> {
+        return recordRepository.getRecordInReview(goalId: goalId, requestValue: requestValue)
+        
+    }
+}
