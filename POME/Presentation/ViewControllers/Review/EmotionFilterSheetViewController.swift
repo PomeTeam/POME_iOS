2bf262507a5c21a11af240a9dc3b12f1b87f40c9
SSOOYA <bsy24660@naver.com>
2023-05-17 16:42:27 +0900

    [chore] EmotionFilterSheetViewController 파일 네이밍에서 Test 제거

--- POME/Presentation/ViewControllers/Review/EmotionFilterSheetViewController.swift
+++ POME/Presentation/ViewControllers/Review/EmotionFilterSheetViewController.swift
@@ -1,5 +1,5 @@
 //
-//  EmotionFilterSheetTestViewController.swift
+//  EmotionFilterSheetViewController.swift
 //  POME
 //
 //  Created by 박소윤 on 2023/04/05.
--- POME/Presentation/ViewControllers/Review/ReviewViewController.swift
+++ POME/Presentation/ViewControllers/Review/ReviewViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ReviewTestViewController.swift
+//  ReviewViewController.swift
 //  POME
 //
 //  Created by 박소윤 on 2023/04/03.
